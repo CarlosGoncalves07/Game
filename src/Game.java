c4b9b1e4b39dc71c87cf4d7f2e1faf526d778d75
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-10-02 17:05:05 -0300

    Game

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+import br.senai.br.jandira.model.Menu;
+
+public class Game {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.executarMenu();
+    }
+
+}
